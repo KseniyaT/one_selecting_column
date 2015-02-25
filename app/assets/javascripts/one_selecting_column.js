586cecfa45f4f369e9421ee9a147322f4fb3763d
axi.traene <axi.traene@gmail.com>
2015-02-25 16:00:58 +0400

    selecting columns plugin

---
+++ app/assets/javascripts/one_selecting_column.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  var hovered_table_td = $('table.table td');
+  hovered_table_td.selecting_column();
+});
+
+(function($){
+  $.fn.selecting_column = function(options){
+    var defaults = {
+    }
+
+    options = $.extend(defaults, options);
+
+    return this.each(function(){
+      var obj = $(this)
+        ;
+      obj.mouseenter(function(){
+        var hovered_index = obj.index()
+          , this_table = obj.closest('table')
+          ;
+        this_table.find('tr').each(function(){
+          $(this).children().eq(hovered_index).addClass('hovered');
+        })
+      });
+      obj.mouseleave(function(){
+        $('table .hovered').removeClass('hovered');
+      });
+    });
+  }
+})(jQuery);
